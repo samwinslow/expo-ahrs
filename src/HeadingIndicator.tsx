8aa59f82e94cebd85258d9f5cf05dc3a3d12fa3f
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2022-12-13 12:54:07 -0500

    draw ticks

--- src/HeadingIndicator.tsx
+++ src/HeadingIndicator.tsx
@@ -1,11 +1,36 @@
-import { Canvas, Circle, Line, useComputedValue, useValue, vec } from '@shopify/react-native-skia'
+import { Canvas, Circle, Group, Line, Points, SkPoint, useComputedValue, useValue, vec } from '@shopify/react-native-skia'
 import { FC, useMemo } from 'react'
 import { useWindowDimensions, View } from 'react-native'
-import { cosd, sind } from './utils'
+import { cosd, degToRad, sind } from './utils'
+import { range as _range } from 'lodash'
 
 type HeadingIndicatorProps = {
   heading: number
   track: number
+}
+
+const getTick = (origin: SkPoint, radius: number, angle: number) => {
+  const length = getTickLength(angle)
+  const x1 = origin.x + radius * sind(angle)
+  const y1 = origin.y - radius * cosd(angle)
+  const x2 = origin.x + (radius - length) * sind(angle)
+  const y2 = origin.y - (radius - length) * cosd(angle)
+  return [vec(x1, y1), vec(x2, y2)]
+}
+
+const getTickLength = (angle: number) => {
+  switch (true) {
+    case angle % 90 === 0:
+      return 32
+    case angle % 30 === 0:
+      return 24
+    case angle % 10 === 0:
+      return 18
+    case angle % 5 === 0:
+      return 12
+    default:
+      return 8
+  }
 }
 
 const HeadingIndicator: FC<HeadingIndicatorProps> = ({ heading, track = 0 }) => {
@@ -37,9 +62,17 @@ const HeadingIndicator: FC<HeadingIndicatorProps> = ({ heading, track = 0 }) => 
     return vec(x, y)
   }, [track, radius.current, origin.current])
 
+  const cardinalPoints = useMemo(() => {
+    const { x, y } = origin.current
+    const r = radius.current
+
+    return _range(0, 360).flatMap(angle => getTick(origin.current, radius.current, angle))
+  }, [origin.current, radius.current])
+
   return (
     <View style={viewStyle}>
       <Canvas style={{ flex: 1 }}>
+        <Group origin={origin} transform={[{ rotate: degToRad(-heading) }]}>
         <Circle
           cx={origin.current.x}
           cy={origin.current.y}
@@ -56,10 +89,18 @@ const HeadingIndicator: FC<HeadingIndicatorProps> = ({ heading, track = 0 }) => 
         <Line
           p1={origin}
           p2={headingPoint}
-          color="#FFF"
+          color="#22FF00"
           style="stroke"
           strokeWidth={2}
         />
+        <Points
+          points={cardinalPoints}
+          mode="lines"
+          color="#FFF"
+          style="stroke"
+          strokeWidth={1}
+        />
+        </Group>
       </Canvas>
     </View>
   )
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
-export const sind = (deg: number) => Math.sin(deg * Math.PI / 180)
-export const cosd = (deg: number) => Math.cos(deg * Math.PI / 180)
+export const degToRad = (deg: number) => deg * Math.PI / 180
+export const sind = (deg: number) => Math.sin(degToRad(deg))
+export const cosd = (deg: number) => Math.cos(degToRad(deg))
 export function isDefined<T>(value: T | undefined | null): value is T {
   return value !== undefined && value !== null
 }
